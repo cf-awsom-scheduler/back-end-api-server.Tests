3fd832363f51882f35818b927c397edd33faea60
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-07-18 15:14:45 -0700

    add unit test project and gitignore

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace back_end_api_server.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
